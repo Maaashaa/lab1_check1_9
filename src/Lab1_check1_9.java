273b1002514b620937aa3c647f2ffa4fa3555853
Maaashaa <mabolotova@mail.ru>
2021-11-14 17:50:46 +0300

    Initial commit

---
+++ src/Lab1_check1_9.java
@@ -0,0 +1,35 @@
+public class Lab1_check1_9 {
+
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        boolean result_1;
+        boolean result_2;
+        boolean result_3;
+        boolean result_4;
+        boolean result_5;
+        boolean result_6;
+        boolean result_7;
+        boolean result_8;
+        boolean result_9 = false;
+	result_1= a==b;
+    result_2 = a!=b;
+    result_3= a<b;
+    result_4= a>b;
+    result_5 = a<=4;
+    result_6 = b>=6;
+    result_7 = a>b || b<a;
+    result_8 = 3<a && a<6;
+        boolean b1 = result_9 != result_9;
+
+        System.out.println(result_1);
+        System.out.println(result_2);
+        System.out.println(result_3);
+        System.out.println(result_4);
+        System.out.println(result_5);
+        System.out.println(result_6);
+        System.out.println(result_7);
+        System.out.println(result_8);
+        System.out.println(result_9);
+    }
+}
